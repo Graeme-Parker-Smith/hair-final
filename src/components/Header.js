25c7f478041179c06e6ef0950140d0f3810df4e8
Graeme <graemster@gmail.com>
2020-09-24 14:53:44 -0500

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+export default () => {
+	return (
+		<header className="header">
+			<div>Gordiany's Hair Salon</div>
+			<div>When only the best will do</div>
+		</header>
+	);
+};
