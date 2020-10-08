7dc353e9de2fc4a078a1572620d7958a7d198e35
Graeme <graemster@gmail.com>
2020-10-08 18:28:03 -0500

    finish contact page

--- src/screens/Contact/Contact.js
+++ src/screens/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CoolButton from '../../components/CoolButton/CoolButton';
 import './Contact.css';
 
 export default () => {
@@ -49,7 +50,7 @@ export default () => {
 							<span>Email:</span>
 							<span>*</span>
 						</div>
-						<input type="text" id="email" name="email" />
+						<input type="email" id="email" name="email" />
 						<br />
 						<label for="email"></label>
 						<br />
@@ -60,29 +61,53 @@ export default () => {
 								<span>*</span>
 							</div>
 							<div>
-								<input type="text" id="area-code" name="area-code" />
+								<input type="tel" id="area-code" name="area-code" />
 								<br />
-								<label className="mini-label" for="area-code">Area Code</label>
+								<label className="mini-label" for="area-code">
+									Area Code
+								</label>
 							</div>
 							{/* <br /> */}
 							<div>-</div>
 							<div>
 								<input type="text" id="number" name="number" />
 								<br />
-								<label className="mini-label" for="number">Phone Number</label>
+								<label className="mini-label" for="number">
+									Phone Number
+								</label>
 								<br />
 							</div>
 						</div>
 
-						<input type="text" id="email" name="email" />
 						<br />
-						<label for="email"></label>
+						<div className="comment-container">
+							<div className="names-container-label">
+								<span>How can we help?</span>
+								<span>*</span>
+							</div>
+							<textarea id="comment" name="comment"></textarea>
+						</div>
 						<br />
-						<input type="text" id="email" name="email" />
-						<br />
-						<label for="email"></label>
-						<br />
+
+						<div className="preference">
+							<span>Best Time to Call:</span>
+							<div>
+								<div>
+									<label>Mornings</label>
+									<input type="checkbox" />
+								</div>
+								<div>
+									<label>Evenings</label>
+									<input type="checkbox" />
+								</div>
+							</div>
+						</div>
 					</form>
+					<div className="submit">
+						{/* <input type="submit" value="Submit" /> */}
+						<CoolButton text="Submit" />
+						<CoolButton text="Reset" />
+					</div>
 				</div>
 			</div>
 		</div>
