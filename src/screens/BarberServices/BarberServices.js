11bf2188efb8d17c817ddca3b63932cf6b0e3850
Graeme <graemster@gmail.com>
2020-10-15 23:20:11 -0500

    update barber

--- src/screens/BarberServices/BarberServices.js
+++ src/screens/BarberServices/BarberServices.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './BarberServices.css';
 
-
 export default () => {
 	return (
 		<div id="testimonials-container" className="screen-content">
+			<div className="videos-header">
+				<img id="film-roll" alt="film roll" src={require('../../images/scissors-52x50.png')} />
+				<h1>Barber Services</h1>
+				<h4>Any Haircut from Past to Present</h4>
+				<p>
+					At Gordiany’s Hair Salon we have a rare talent on our staff of fine hairstylists. Larysa Gordiany,
+					co-owner and Master Barber Stylist, has added a host of men’s barber services. She can create any
+					style for all ages. We invite you to drop by our barber studio located in beautiful Los Patios in
+					San Antonio. We carry Johnny B and American Crew products for the best style possible.
+				</p>
+			</div>
 			<div>
 				<h1>What Our Clients Say</h1>
 				<p>
@@ -20,12 +30,9 @@ export default () => {
 					about the extraordinary services provided at Gordiany Hair Salon.
 				</p>
 				<p>Give us a call and make an appointment to come in see the difference.</p>
-			
 			</div>
 			<div className="testimonials-col-2">
-				<div className="contact-form-container">
-				
-				</div>
+				<div className="contact-form-container"></div>
 			</div>
 		</div>
 	);
