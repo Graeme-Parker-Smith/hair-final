5953b15f2aa5fca0539073edb179b9c956f17cd1
Graeme <graemster@gmail.com>
2020-10-26 23:35:06 -0500

    update card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,17 +8,18 @@ export default ({ imgName = false, header = 'Hello', text = 'Hello', hasButton =
 
 	useEffect(() => {
 		AOS.init({
-			duration: 2000,
-			easing: 'ease-in-out-back'
+			duration: 1500,
+			mirror: true,
+			offset: 150
 		});
 		AOS.refresh();
 	}, [])
 	return (
-		<div data-aos="fade-up" id={id} data-aos-anchor={`#${id}`} data-aos-anchor-placement="top-center" className="card">
+		<div data-aos="fade-up" id={id}  className="card">
 			{imgName && <img className="card-img" src={require(`../../images/${imgName}.jpg`)} alt="beautiful hair" />}
 			<h4 style={{fontSize: headerSize, fontWeight: headerWeight}}>{header}</h4>
 			<p>{text}</p>
-			{/* <div className="btn-outline" >Learn More</div> */}
+			
 			{hasButton && <CoolButton />}
 		</div>
 	);
